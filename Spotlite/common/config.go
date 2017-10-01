318dd7707a539818511b435e41b57fc6befa6c04
ganesh avalur <ganeshavalur@gmail.com>
2017-10-01 12:12:55 -0700

    Added producer topic and consumer topics

--- Spotlite/common/config.go
+++ Spotlite/common/config.go
@@ -1,17 +1,19 @@
 package insight
 
 type config struct {
-	KafkaBrokers      string   `json:"kafka_brokers"`
-	CassandraClusters []string `json:"cassandra_clusters"`
-	RedisClusters     string   `json:"redis_clusters"`
-	KafkaTopics       string   `json:"kafka_topics"`
-	KafkaGroup        string   `json:"kafka_groups"`
+	KafkaBrokers       string   `json:"kafka_brokers"`
+	CassandraClusters  []string `json:"cassandra_clusters"`
+	RedisClusters      string   `json:"redis_clusters"`
+	KafkaTopics        []string `json:"kafka_topics"`
+	KafkaProducerTopic string   `json:"kafka_producer_topic"`
+	KafkaGroup         string   `json:"kafka_groups"`
 }
 
 var DefaultConfig = config{
-	KafkaBrokers:      "ec2-52-11-70-58.us-west-2.compute.amazonaws.com",
-	CassandraClusters: []string{"127.0.0.1"},
-	RedisClusters:     "127.0.0.1:6379",
-	KafkaTopics:       "jaja",
-	KafkaGroup:        "0",
+	KafkaBrokers:       "ec2-52-11-70-58.us-west-2.compute.amazonaws.com",
+	CassandraClusters:  []string{"127.0.0.1"},
+	RedisClusters:      "127.0.0.1:6379",
+	KafkaTopics:        []string{"drevents"},
+	KafkaProducerTopic: "drevents",
+	KafkaGroup:         "0",
 }
